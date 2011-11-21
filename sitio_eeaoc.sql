7faaab88fe29093df3a4bb4e7ca14f62388424c1
pedrodaniel <pdrc83@gmail.com>
2011-11-21 20:43:11 -0300

    Nuvo backup de la base

--- sitio_eeaoc.sql
+++ sitio_eeaoc.sql
@@ -78,7 +78,7 @@ CREATE TABLE `modulos` (
   `accion` varchar(50) default NULL,
   `hijos` int(1) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 #
 # Structure for the `novedades` table : 
@@ -104,7 +104,7 @@ CREATE TABLE `novedades` (
 
 CREATE TABLE `pagina` (
   `id` int(11) NOT NULL auto_increment,
-  `titulo` int(11) default NULL,
+  `titulo` varchar(50) default NULL,
   `orden` int(11) default NULL,
   `imagen` varchar(50) default NULL,
   `padre_id` int(11) default '0',
@@ -112,7 +112,7 @@ CREATE TABLE `pagina` (
   `tipo` int(11) default NULL COMMENT '1- Menu principal\r\n2- Submenu\r\n3- Tercer Menu',
   `accion` varchar(50) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 #
 # Structure for the `perfil` table : 
@@ -122,8 +122,9 @@ CREATE TABLE `perfil` (
   `id` int(11) NOT NULL auto_increment,
   `perfil` varchar(20) default NULL,
   `habilitado` tinyint(1) default '1',
+  `descripcion` text,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 #
 # Structure for the `permiso` table : 
@@ -133,12 +134,12 @@ CREATE TABLE `permiso` (
   `id` int(11) NOT NULL auto_increment,
   `perfil_id` int(11) default NULL,
   `modulo_id` int(11) default NULL,
-  `Alta` int(1) default NULL,
-  `Baja` int(1) default NULL,
-  `Modificacion` int(1) default NULL,
-  `Listado` int(1) default NULL,
+  `Alta` int(1) default '0',
+  `Baja` int(1) default '0',
+  `Modificacion` int(1) default '0',
+  `Listado` int(1) default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
 
 #
 # Structure for the `producto` table : 
@@ -214,6 +215,8 @@ CREATE TABLE `tematica` (
   `imagen` varchar(50) default NULL,
   `padre_id` int(11) default NULL,
   `hijos` tinyint(1) default NULL,
+  `fecha_carga` datetime default NULL,
+  `usuario_id` int(11) default NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -242,7 +245,7 @@ CREATE TABLE `usuario` (
   `fecha_alta` datetime default NULL,
   `ultimo_acceso` datetime default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 #
 # Data for the `modulos` table  (LIMIT 0,500)
@@ -252,9 +255,27 @@ INSERT INTO `modulos` (`id`, `nombre`, `orden`, `padre_id`, `menu`, `accion`, `h
   (1,'Administración',1,0,1,'usuarios',1),
   (2,'Usuarios',1,1,1,'usuarios',0),
   (3,'Perfiles',2,1,1,'perfiles',0),
-  (4,'Permisos',3,1,1,'permisos',0),
-  (5,'Módulos',4,1,1,'modulos',0),
-  (6,'Tematicas',2,0,1,'tematicas',0);
+  (4,'Permisos',0,1,0,'permisos',0),
+  (5,'Módulos',3,1,1,'modulos',0),
+  (6,'Tematicas',2,0,1,'tematicas',1),
+  (11,'Productos',3,0,1,'productos',0),
+  (12,'Rubros',4,0,1,'rubros',0),
+  (13,'Caracteristicas',5,0,1,'paginas',0),
+  (14,'Servicios',6,0,1,'servicios',0),
+  (15,'Contenido',7,0,1,'paginas',1),
+  (16,'Páginas',1,15,1,'paginas',0);
+
+COMMIT;
+
+#
+# Data for the `pagina` table  (LIMIT 0,500)
+#
+
+INSERT INTO `pagina` (`id`, `titulo`, `orden`, `imagen`, `padre_id`, `contenido`, `tipo`, `accion`) VALUES 
+  (1,'Qué es la EEAOC',1,NULL,0,NULL,1,'la-eeaoc'),
+  (2,'Productos',2,NULL,0,NULL,1,'productos'),
+  (3,'Servicios',3,NULL,0,'',1,'servicios'),
+  (4,'Informes Publicaciones',4,NULL,0,NULL,1,'informes-publicaciones');
 
 COMMIT;
 
@@ -262,8 +283,10 @@ COMMIT;
 # Data for the `perfil` table  (LIMIT 0,500)
 #
 
-INSERT INTO `perfil` (`id`, `perfil`, `habilitado`) VALUES 
-  (1,'Administrador',1);
+INSERT INTO `perfil` (`id`, `perfil`, `habilitado`, `descripcion`) VALUES 
+  (1,'Desarrollador',1,'Perfl de testing para los Usuarios desarrolladores del sistema'),
+  (2,'Administrador',1,'Usuarios encargados de administrar la información brindada por el sitio'),
+  (3,'Carga',1,'Grupo de usuarios encargados de administrar la información que el sitio mostrará en la web');
 
 COMMIT;
 
@@ -272,12 +295,36 @@ COMMIT;
 #
 
 INSERT INTO `permiso` (`id`, `perfil_id`, `modulo_id`, `Alta`, `Baja`, `Modificacion`, `Listado`) VALUES 
-  (1,1,1,0,0,0,1),
+  (1,1,1,1,1,1,1),
   (2,1,2,1,1,1,1),
   (3,1,3,1,1,1,1),
   (4,1,4,1,1,1,1),
   (5,1,5,1,1,1,1),
-  (6,1,6,1,1,1,1);
+  (6,1,6,1,1,1,1),
+  (16,1,11,1,1,1,1),
+  (17,1,12,1,1,1,1),
+  (12,2,6,1,1,1,1),
+  (18,1,13,1,1,1,1),
+  (19,1,14,1,1,1,1),
+  (20,1,15,1,1,1,1),
+  (21,1,16,1,1,1,1),
+  (22,2,1,0,0,0,1),
+  (23,2,2,1,1,1,1),
+  (24,2,3,1,1,1,1),
+  (25,2,4,1,1,1,1),
+  (33,3,6,1,1,1,1),
+  (27,2,11,1,1,1,1),
+  (28,2,12,1,1,1,1),
+  (29,2,13,1,1,1,1),
+  (30,2,14,1,1,1,1),
+  (31,2,15,0,0,0,1),
+  (32,2,16,1,1,1,1),
+  (34,3,11,1,1,1,1),
+  (35,3,12,1,1,1,1),
+  (36,3,13,1,1,1,1),
+  (41,3,16,1,1,1,1),
+  (38,3,14,1,1,1,1),
+  (40,3,15,0,0,0,1);
 
 COMMIT;
 
@@ -297,7 +344,10 @@ COMMIT;
 #
 
 INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `email`, `password`, `perfil_id`, `habilitado`, `fecha_alta`, `ultimo_acceso`) VALUES 
-  (1,'Pedro Daniel','Romano Correa','pdrc83@gmail.com','32f972344bc7de6a9f0f0f0123883798',1,1,'2011-10-01','2011-11-18 21:03:15');
+  (1,'Pedro Daniel','Romano','pdrc83@gmail.com','32f972344bc7de6a9f0f0f0123883798',1,1,'2011-10-01','2011-11-21 20:54:06'),
+  (2,'Facundo','Ruiz','facundoruiz@hotmail.com','e10adc3949ba59abbe56e057f20f883e',1,1,NULL,NULL),
+  (3,'Analia Gabriela','Mansilla','anitagmansilla@gmail.com','e10adc3949ba59abbe56e057f20f883e',3,1,NULL,'2011-11-20 13:11:40'),
+  (4,'Italo Iván','Ramos','ivan@hotmail.com','e10adc3949ba59abbe56e057f20f883e',2,1,NULL,'2011-11-20 13:27:46');
 
 COMMIT;
 
